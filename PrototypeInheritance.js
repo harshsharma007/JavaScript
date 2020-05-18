c2391b291c05e7962d485f3dea6d8e49bf8a6dfa
Harsh Sharma <harshsharmahs@gmail.com>
2020-05-18 17:19:48 +0530

    JavaScript

---
+++ PrototypeInheritance.js
@@ -0,0 +1,16 @@
+/*
+    Non-primitive types have a few properties/methods associated with them.
+        Array.prototype.push()
+        String.prototype.toUpperCase()
+*/
+
+//Array.push()
+const arr = []
+arr.push('value')
+arr.push('value1')
+
+console.log(arr)
+
+//String.toUpperCase()
+const str = 'test'
+console.log(str.toUpperCase())
---
+++ ShallowAndDeepCopy.js
@@ -0,0 +1,40 @@
+//Shallow Copy
+const o = {
+    a: 'a',
+    b: 'b'
+}
+
+const o2 = Object.assign({}, o)
+//The above line of code will perform shallow copy.
+
+console.log(o2)
+
+//Deep Copy
+function deepCopy(obj) {
+    //check if values are object
+    //if so, copy that object (deep copy)
+    //else return the value
+
+    const keys = Object.keys(obj)
+    const newObject = {}
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i]
+        if (typeof obj[key] === 'object')
+            newObject[key] = deepCopy(obj[key])
+        else
+            newObject[key] = obj[key]
+        return newObject
+    }
+}
+
+const o3 = deepCopy(o)
+//The above line of code will perform deep copy.
+
+console.log(o3)
+o.a = 'New Key'
+console.log(o3.a)
+
+//The value displayed would be 'Key', instead of 'New Key'. This is an example of deep copy.
+
+//Arrays are also stored by reference. In case of deep copy of an array, we have to check === 'array'
