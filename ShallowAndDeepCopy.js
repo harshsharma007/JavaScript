b7f003eb9d4490f1342abb6e694766cb11195c53
Harsh Sharma <harshsharmahs@gmail.com>
2020-05-19 16:27:12 +0530

    Shallow Copy Changes

--- ShallowAndDeepCopy.js
+++ ShallowAndDeepCopy.js
@@ -1,7 +1,10 @@
 //Shallow Copy
 const o = {
     a: 'a',
-    b: 'b'
+    b: 'b',
+    obj: {
+        key: 'key'
+    }
 }
 
 const o2 = Object.assign({}, o)
@@ -20,21 +23,22 @@ function deepCopy(obj) {
 
     for (let i = 0; i < keys.length; i++) {
         const key = keys[i]
-        if (typeof obj[key] === 'object')
+        if (typeof obj[key] === 'object') {
             newObject[key] = deepCopy(obj[key])
-        else
+        }
+        else {
             newObject[key] = obj[key]
-        return newObject
+        }
     }
+    return newObject
 }
 
 const o3 = deepCopy(o)
 //The above line of code will perform deep copy.
 
 console.log(o3)
-o.a = 'New Key'
-console.log(o3.a)
+o.obj.key = 'New Key'
+console.log(o3.obj.key)
 
 //The value displayed would be 'Key', instead of 'New Key'. This is an example of deep copy.
-
 //Arrays are also stored by reference. In case of deep copy of an array, we have to check === 'array'
