8478641cc49a170e145a430119d071f4a5430615
Harsh Sharma <harshsharmahs@gmail.com>
2020-06-09 20:41:17 +0530

    Closures

--- Closures.js
+++ Closures.js
@@ -132,3 +132,188 @@ peter() //Peter
     the variable of its outer function peter even after that function has returned. So the displayName
     function is actually a closure.
 */
+
+//Example #2
+
+function getCounter() {
+    let counter = 0
+
+    return function () {
+        return counter++
+    }
+}
+let count = getCounter()
+
+console.log(count()) //0
+console.log(count()) //1
+console.log(count()) //2
+
+/*
+    Again we are storing the anonymous inner function returned by getCounter function into the count variable.
+    As count function is now a closure, it can access the counter variable of getCounter function even after
+    getCounter() has returned.
+
+    But notice that the value of the counter is not resetting to 0 on each count function call as it usually
+    should.
+
+    That's because, at each call of count(), a new scope for the function is created, but there is only
+    single scope created for getCounter function, because the counter variable is defined in the scope of
+    getCounter(), it would get incremented on each count function call instead of resetting to 0.
+*/
+
+/*
+    How do closures work?
+    Up until now, we have discussed what closures are and their practical examples. Now let's understand
+    how closures really work in JavaScript.
+
+    To really understand how closures work in JavaScript, we have to understand the two most important
+    concepts in JavaScript, that is
+
+    1) Execution Context
+    2) Lexical Envrionment
+*/
+
+/*
+    1) Execution Context
+    An execution context is an abstract environment where the JavaScript code is evaluated and executed.
+    When the global code is executed, it's executed inside the global execution context, and the function
+    code is executed inside the function execution context.
+
+    There can only be one currently running execution context (Because JavaScript is single threaded language),
+    which is managed by a stack data structure known as Execution Stack or Call Stack.
+
+    An execution stack is a stack with LIFO (Last in, First out) structure in which items can only be added
+    or removed from the top of the stack only.
+
+    The currently running execution context will be always on the top of the stack, and when the function
+    which is currently running  completes, its execution context is popped off from the stack and the
+    control reaches to the execution context below it in the stack.
+
+    Let's look at a code snippet to better understand execution context and stack:
+*/
+
+let a = 'Hello World!'  //Global Execution Context
+
+function first() {  //first() Function Execution Context
+    console.log('Inside first function')
+} //Function Execution Context ends here
+
+first()
+console.log('Inside Global Execution Context') //Global Execution Context ends here
+
+/*
+    When this code is executed, the JavaScript engine creates a global execution context to execute the global
+    code, and when it encounters the call to first() function, it creates a new execution context for that
+    function and pushes it to the top of the execution stack.
+
+    When the first() function completes, its execution stack is removed from the stack, and the control
+    reaches to execution context below it, that is, global execution context. So the remaining code in global
+    scope will be executed.
+*/
+
+/*
+    Lexical Environment
+    Every time the JavaScript engine creates an execution context to execute the function or global code, it
+    also creates a new lexical environment to store the variable defined in that function during the execution
+    of that function.
+
+    A lexical environment is a data structure that holds identifier-variable mapping. (here identifier refers
+    to the name of variables/functions, and the variable is the reference to actual object [including function
+    type object] or primitive value).
+
+    A Lexical Environment has two components:
+    1) the environment record
+    2) a reference to the outer environment.
+
+    1) The environment record is the actual place where the variable and function declarations are stored.
+    2) The reference to the outer environment means it has access to its outer (parent) lexical environment.
+       This component is the most important in order to understand how closures work.
+    
+    A lexical environment conceptually looks like this:
+
+    lexicalEnvironment = {
+        environmentRecord: {
+            <identifier> : <value>,
+            <identifier> : <value>
+        }
+        outer: <Reference to the parent lexical environment>
+    }
+
+    So, let's again take a look at above code snippet:
+
+    let a = 'Hello World!';
+    function first() {
+        let b = 25;  
+        console.log('Inside first function');
+    }
+    
+    first();
+    console.log('Inside global execution context');
+
+    When the JavaScript engine creates a global execution context to execute global code, it also creates a
+    new lexical environment to store the variables and functions defined in the global scope. So the lexical
+    environment for the global scope will look like this:
+
+    globalLexicalEnvironment = {
+        environmentRecord: {
+            a : 'Hello World!',
+            first : <reference to function object>
+        }
+        outer: null
+    }
+
+    Here the outer lexical environment is set to null because there is no outer lexical environment for the
+    global scope.
+
+    When the engine creates execution context for first() function, it also creates a lexical environment to
+    store variables defined in that function during execution of the function. So the lexical environment of
+    the function will look like this:
+
+    functionLexicalEnvironment = {
+        environmentRecord: {
+            b : 25,
+        }
+        outer: <globalLexicalEnvironment>
+    }
+
+    Note - When a function completes, its execution context is removed from the stack, but its lexical
+    environment may or may not be removed from the memory depending on if that lexical environment is
+    referenced by any other lexical environments in their outer lexical environment property.
+*/
+
+/*
+    Detailed Closures Examples
+    Now that we understand the execution context and lexical environment, let's get back to the closures.
+
+    Example #1
+    Take a look at this code snippet:
+
+    function person() {
+        let name = 'Peter';
+
+        return function displayName() {
+            console.log(name);
+        };
+    }
+    
+    let peter = person();
+    peter(); // prints 'Peter'
+
+    When the person function is executed, the JavaScript engine creates a new execution context and lexical
+    environment for the function. After this function finishes, it returns displayName function and assigns
+    it to peter variable.
+
+    So, its lexical environment will look like this:
+
+    personLexicalEnvironment = {
+        environmentRecord: {
+            name : 'Peter',
+            displayName : <displayName function reference>
+        }
+        outer: <globalLexicalEnvironment>
+    }
+
+    When the person function finishes, its execution context is removed from the stack. But its lexical
+    environment is still in the memory because its lexical environment is referenced by the lexical environment
+    of its inner displayName function. So its variables are still available in the memory.
+*/
