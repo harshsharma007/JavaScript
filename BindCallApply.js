9893b826c3716e50b43bdcca6e698a7b4d794ae5
Harsh Sharma <harshsharmahs@gmail.com>
2020-07-24 15:39:50 +0530

    Bind Call Apply

--- BindCallApply.js
+++ BindCallApply.js
@@ -1,11 +1,11 @@
 /*
-    1. this refers to an object that's set at the creation of a new execution context (function invocation).
+    1. "this" refers to an object that's set at the creation of a new execution context (function invocation).
        In other words, another stack frame.
-    2. this in JavaScript is slightly different from this in other languages.
-    3. In the global execution context, this refers to global object.
-    4. If the function is called as a method of an object, 'this' is bound to the object the method is called on.
+    2. "this" in JavaScript is slightly different from "this" in other languages.
+    3. In the global execution context, "this" refers to global object.
+    4. If the function is called as a method of an object, "this" is bound to the object the method is called on.
        A method is a term for a key value pair in an object where the value is a function. That key is
-       considered a method. So, if a function is called as a method of an object, this is bound to that object
+       considered a method. So, if a function is called as a method of an object, "this" is bound to that object
        that function is called on.
 */
 
@@ -17,7 +17,7 @@ const person = {
 }
 
 /*
-   this is a part of a function which is defined as a method on this object.
+   "this" is a part of a function which is defined as a method on this object.
    Method is a key where the value is a function.
    In this case, person.greet is considered a method because greet is this function.
 */
@@ -32,15 +32,15 @@ const greet = person.greet
 greet() //Case 2
 
 /*
-   What would be the value of this for Case 2?
+   What would be the value of "this" for Case 2?
    Now greet is a global object. So the value of this.name would be undefined. Because name is not a key on
    that global object. And, when we try to de-reference a key which is not defined globally we get undefined.
 */
 
 name = 'Harmony'
 /*
-   this refers to global object. If console.log(this.name) is executed on the browser it will return value
-   of the name 'Harmony'. Since, this is not a global object in VS code or Node.js. So, we have to use
+   "this" refers to global object. If console.log(this.name) is executed on the browser it will return value
+   of the name 'Harmony'. Since, "this" is not a global object in VS code or Node.js. So, we have to use
    window object to fetch the value of name variable.
 */
 console.log(this.name)
@@ -70,15 +70,15 @@ person.greet.apply({ name: 'this is a bound object' })
    Difference between Bind, Call and Apply?
    Bind returns a new function.
    Call and Apply rather than returning a new function they will immediately invoke that function.
-   E.g. person.greet.bind returns a new function where this is automatically bound.
+   E.g. person.greet.bind returns a new function where "this" is automatically bound.
 
    Bind returns a new function which we store in greet and invoke greet later.
    Call and Apply immediately invoked that.
 */
 
 /*
-   Other way to set this manually is by using ES6 arrow notation. ES6 arrow notation will bind this to be
-   whatever this is at the time we have declared the function rather than at the time we have invoked the
+   Other way to set "this" manually is by using ES6 arrow notation. ES6 arrow notation will bind "this" to be
+   whatever "this" is at the time we have declared the function rather than at the time we have invoked the
    function.
 */
 
@@ -91,7 +91,7 @@ newPerson.greet()
 
 /*
    newPerson.greet() will console log 'undefined'. Why?
-   Because this is a global object. ES6 arrow notation will bind this to be whatever the value is at the time of writing.
+   Because "this" is a global object. ES6 arrow notation will bind "this" to be whatever the value is at the time of writing.
 */
 
 /*
